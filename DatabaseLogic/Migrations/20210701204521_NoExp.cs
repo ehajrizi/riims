8f6574528a439b55e3ac98f89d1b8b20f9500d8c
ehajrizi <73657577+ehajrizi@users.noreply.github.com>
2021-07-01 23:09:55 +0200

    Eksperienca_DateFix

---
+++ DatabaseLogic/Migrations/20210701204521_NoExp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatabaseLogic.Migrations
+{
+    public partial class NoExp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eksperiencat");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Eksperiencat",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DataFillestare = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataPerfundimtare = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    EmriInstitucionit = table.Column<string>(type: "TEXT", nullable: true),
+                    Lokacioni = table.Column<string>(type: "TEXT", nullable: true),
+                    NumriTelefonit = table.Column<string>(type: "TEXT", nullable: true),
+                    Pershkrimi = table.Column<string>(type: "TEXT", nullable: true),
+                    PersoniKontaktues = table.Column<string>(type: "TEXT", nullable: true),
+                    PunePrimare = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Titulli = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eksperiencat", x => x.Id);
+                });
+        }
+    }
+}
---
+++ DatabaseLogic/Migrations/20210701205455_Fix_Eksp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatabaseLogic.Migrations
+{
+    public partial class Fix_Eksp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Eksperiencat",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    EmriInstitucionit = table.Column<string>(type: "TEXT", nullable: true),
+                    Titulli = table.Column<string>(type: "TEXT", nullable: true),
+                    PunePrimare = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Lokacioni = table.Column<string>(type: "TEXT", nullable: true),
+                    DataFillestare = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataPerfundimtare = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Pershkrimi = table.Column<string>(type: "TEXT", nullable: true),
+                    PersoniKontaktues = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    NumriTelefonit = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eksperiencat", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eksperiencat");
+        }
+    }
+}
